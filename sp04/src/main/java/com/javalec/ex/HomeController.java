63143aeb576d1450df3daea5585800128ab7af66
eventia <eventia@gmail.com>
2018-05-06 20:07:22 +0900

    Do Practice

--- sp04/src/main/java/com/javalec/ex/HomeController.java
+++ sp04/src/main/java/com/javalec/ex/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@Controller // 어노테이션 Controller 는 스프링에서 관리
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
